4f3310712de03a0d58258c589d4c7437c45d031e
root <antran2499@gmail.com>
2022-10-17 08:21:41 +0000

    Update ETL Save data function

--- data/process_data.py
+++ data/process_data.py
@@ -44,7 +44,7 @@ def clean_data(df):
 
 def save_data(df, database_filename):
     engine = create_engine(f'sqlite:///{database_filename}')
-    df.to_sql(database_filename, engine, index=False)  
+    df.to_sql('DisasterResponse', engine, index=False)  
 
 
 def main():
